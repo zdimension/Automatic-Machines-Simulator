57fc150e5a7d0d771097e6ee6d4c72bef70e8543
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-10-17 00:38:06 +0200

    Detect duplicate states

--- am/commands/analysis.py
+++ am/commands/analysis.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-from am.commands import cmd
+from collections import defaultdict
 
+from am.commands import cmd
+from itertools import groupby
 
 @cmd()
 def analysis(am, **kwargs):
@@ -10,3 +12,14 @@ def analysis(am, **kwargs):
     print("Analysis of machine:", am.name)
     reached = {ns for state, transitions in am.transitions.items() for w, m, ns in transitions.values() if ns != state}
     print("Unreachable states:", ", ".join(set(am.transitions) - {am.initial_state} - reached))
+    cache = {}
+    dupl = {}
+    for state, transitions in am.transitions.items():
+        fs = frozenset(transitions)
+        if fs in cache:
+            if fs not in dupl:
+                dupl[fs] = [cache[fs]]
+            dupl[fs].append(state)
+        else:
+            cache[fs] = state
+    print("Duplicate states (can be merged):", ", ".join("(" + ", ".join(x) + ")" for x in dupl.values()))
